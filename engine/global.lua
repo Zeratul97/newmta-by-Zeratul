7d6a498970c74244875a5396eb767202d6f3d6b5
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 17:26:50 +0100

    Update global.lua

--- engine/global.lua
+++ engine/global.lua
@@ -1,53 +1,53 @@
 modData = {
-	["name"] = "NewMTA",
-	["version"] = "v.0.0.1",
-	["city"] = "Los Santos",
+    ["name"] = "NewMTA",
+    ["version"] = "v.0.0.1",
+    ["city"] = "Los Santos",
 }
 
 jobNames = {
-	[0] = "Munkanélküli",
+    [0] = "Munkanélküli",
 }
 
 vehicleNames = {
-	[596] = "Opel Corsa",
+    [596] = "Opel Corsa",
 }
 
 -- Járművek nevének átküldése --
 function vehicleNamesService(vehicle)
-	if (vehicle) then
-		if (vehicleNames[getElementModel(vehicle)]) then
-			return vehicleNames[getElementModel(vehicle)]
-		else
-			return "ISMERETLEN"
-		end
+    if (vehicle) then
+	if (vehicleNames[getElementModel(vehicle)]) then
+	    return vehicleNames[getElementModel(vehicle)]
 	else
-		return false
+	    return "ISMERETLEN"
 	end
+    else
+	return false
+    end
 end
 
 -- Munkák nevének átküldése --
 function jobNamesService(player)
-	if (player) then
-		if (jobNames[getElementData(player, "job >> id")]) then
-			return jobNames[getElementData(player, "job >> id")]
-		else
-			return false
-		end
+    if (player) then
+	if (jobNames[getElementData(player, "job >> id")]) then
+	    return jobNames[getElementData(player, "job >> id")]
 	else
-		return false
+	    return false
 	end
+    else
+	return false
+    end
 end
 
 -- Mod Adatok elküldése --
 function modDatas(data)
-	if (data) then
-		if (modData[data]) then
-			return modData[data]
-		else
-			return false
-		end
+    if (data) then
+	if (modData[data]) then
+	    return modData[data]
 	else
-		return false
+	    return false
 	end
+    else
+	return false
+    end
 end
 
