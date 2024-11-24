4d9a70036f48af5ccfd5cf62647eb53409e3800a
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 18:17:28 +0100

    Update infoboxServer.lua

--- nwmta_infobox/infoboxServer.lua
+++ nwmta_infobox/infoboxServer.lua
@@ -1,5 +1,5 @@
 function createInfoBox(player, message, itype)
-	if (isElement(player)) then
-		triggerClientEvent(getRootElement(), "infobox", message, itype)
-	end
+    if (isElement(player)) then
+	triggerClientEvent(getRootElement(), "infobox", message, itype)
+    end
 end
