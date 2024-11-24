77d52511eb3cfc9e64f66c073162f1478005bd60
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 18:20:34 +0100

    Create global.lua

---
+++ nwmta_whitelist/global.lua
@@ -0,0 +1,18 @@
+-- Fejlesztés idején kik léphetnek fel a szerverre --
+whiteSerailList = {
+	--["serial"] = true,
+	--["serial"] = true,
+}
+
+-- Ellenőrzi a játékos serial-ja megtalálható a listában --
+function getWhiteListSerial(player)
+	if (player) then
+		if (whiteSerailList[getPlayerSerial(player)]) then
+			return true
+		else
+			return false
+		end
+	else
+		return false
+	end	
+end
