bd9ae79b5e2f79ebfc5794b48073f0e542717cc1
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 17:54:28 +0100

    Update global.lua

--- nwmta_dev/global.lua
+++ nwmta_dev/global.lua
@@ -1,68 +1,67 @@
 -- Tiltott parancsok --
 disableCMD = {
-	["register"] = true,
-	["login"] = true,
-	["refreshall"] = true,
-	["stopall"] = true,
+     ["register"] = true,
+     ["login"] = true,
+     ["refreshall"] = true,
+     ["stopall"] = true,
 }
 
 -- Fejlesztők serial-ja 
 devSerail = {
-	--["serial"] = true,
-	["3EEE639776EC134366D2D005508912F3"] = true,
+     --["serial"] = true, -- Fejlesztő serial-ja kerüljön a "serial" helyére, amennyiben több fejlesztő van akkor ugyan így adj hozzá több sort.
 }
 
 -- Fejlesztői parancsok listálya --
 devCMD = {
-	-- Alap fejlesztői parancsok --
-	["start"] = true,
-	["stop"] = true,
-	["refresh"] = true,
-	["restart"] = true,
-	["debugscript"] = true,
-	["reloadacl"] = true,
+     -- Alap fejlesztői parancsok --
+     ["start"] = true,
+     ["stop"] = true,
+     ["refresh"] = true,
+     ["restart"] = true,
+     ["debugscript"] = true,
+     ["reloadacl"] = true,
 
-	-- Új fejlesztői parancsok --
-	["getpos"] = true,
-	["enableddevmode"] = true,
-	["enabledcollisions"] = true,
+     -- Új fejlesztői parancsok --
+     ["getpos"] = true,
+     ["enableddevmode"] = true,
+     ["enabledcollisions"] = true,
 }
 
 -- Fejlesztői serial ellenőrző funkció --
 function getDevSerials(player)
-	if (player) then
-		if (devSerail[getPlayerSerial(player)]) then
-			return true
-		else
-			return false
-		end
+    if (player) then
+	if (devSerail[getPlayerSerial(player)]) then
+	    return true
 	else
-		return false 
+	    return false
 	end
+    else
+	return false 
+    end
 end
 
 -- Tiltott parancsokat ellenőrző funkció --
 function getCMDDisabled(cmd)
-	if (cmd) then
-		if (disableCMD[cmd]) then
-			return true
-		else
-			return false
-		end
+    if (cmd) then
+	if (disableCMD[cmd]) then
+	    return true
 	else
-		return false
+	    return false
 	end
+    else
+	return false
+    end
 end
 
 -- Fejlesztői parancsokat ellenőrző funkció --
 function getDeveloperCMD(cmd)
-	if (cmd) then
-		if (devCMD[cmd]) then
-			return true
-		else
-			return false
-		end
+    if (cmd) then
+	if (devCMD[cmd]) then
+	    return true
 	else
-		return false
+	    return false
 	end
+    else
+	return false
+    end
 end
