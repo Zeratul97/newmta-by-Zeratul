e2f8f781591ff8538188f91370d88996054312e2
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 18:01:22 +0100

    Update global.lua

--- nwmta_engine/global.lua
+++ nwmta_engine/global.lua
@@ -1,52 +1,52 @@
 -- Mod alap adatai --
 modDatas = {
-	["name"] = "NEWMTA",
-	["version"] = "v0.0.0",
-	["city"] = "Los Santos",
+    ["name"] = "NEWMTA",
+    ["version"] = "v0.0.0",
+    ["city"] = "Los Santos",
 }
 
 jobNames = {
-	[0] = "Munkanélküli",
+    [0] = "Munkanélküli",
 }
 
 vehicleNames = {
-	[596] = "Opel Corsa",
+    [596] = "Opel Corsa",
 }
 
 function vehicleNamesService(vehicle)
-	if (vehicle) then
-		if (vehicleNames[getElementModel(vehicle)]) then
-			return vehicleNames[getElementModel(vehicle)]
-		else
-			return "ISMERETLEN"
-		end
+    if (vehicle) then
+	if (vehicleNames[getElementModel(vehicle)]) then
+	    return vehicleNames[getElementModel(vehicle)]
 	else
-		return false
+	    return "ISMERETLEN"
 	end
+    else
+	return false
+    end
 end
 
 
 function jobNamesService(player)
-	if (player) then
-		if (jobNames[getElementData(player, "job >> id")]) then
-			return jobNames[getElementData(player, "job >> id")]
-		else
-			return false
-		end
+    if (player) then
+	if (jobNames[getElementData(player, "job >> id")]) then
+	    return jobNames[getElementData(player, "job >> id")]
 	else
-		return false
+	    return false
 	end
+    else
+	return false
+    end
 end
 
 -- Mod alap adatainak átküldése --
 function modDataService(data)
-	if (data) then -- Ha kap adatot akkor
-		if (modDatas[data]) then -- Ha az adat megtalálható az adat akkor
-			return modDatas[data]
-		else
-			return false
-		end
+    if (data) then -- Ha kap adatot akkor
+	if (modDatas[data]) then -- Ha az adat megtalálható az adat akkor
+	    return modDatas[data]
 	else
-		return false -- Ha nem kap adatot akkor hamis értéket küld át!
+	    return false
 	end
+    else
+	return false -- Ha nem kap adatot akkor hamis értéket küld át!
+    end
 end
