8394545386e05a4b2fe769285a2de0a1c88ba6b6
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 17:46:00 +0100

    Update global.lua

--- nwmta_banlist/global.lua
+++ nwmta_banlist/global.lua
@@ -1,17 +1,17 @@
 -- Véglegesen kitiltott játékosok serial-ja, ebbe a listába kerül akkor kirúgja a rendszer --
 banSerials = {
-	  ["serial"] = true,
+    ["serial"] = true,
 }
 
 -- Lekérdezi hogy a játékos serial-ja megtalálható -e a listában --
 function banSerialList(player)
-	  if (player) then
-		    if (banSerials[getPlayerSerial(player)]) then
-			      return true
-		    else
-			      return false
-		    end
-	  else
-		    return false
-	  end
+    if (player) then
+	if (banSerials[getPlayerSerial(player)]) then
+	    return true
+	else
+	    return false
+	end
+    else
+	return false
+    end
 end
