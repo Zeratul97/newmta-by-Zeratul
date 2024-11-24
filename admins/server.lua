8a4e72add5e7d33f5931f6c1710da1ab03e91208
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 17:12:28 +0100

    Update server.lua

--- admins/server.lua
+++ admins/server.lua
@@ -1,10 +1,1 @@
-addEventHandler("onPlayerJoin", getRootElement(),
-	function()
-		if (getPlayerSerial(source) == "serial") then -- Zeratul serial
-			setElementData(source, "admin >> level", 11)
-			setElementData(source, "admin", true)
-			setElementData(source, "admin >> nick", "Zeratul")
-			outputChatBox("Szia Zeratul! Tessék az admin rangod. Legyen szép napod! :)", source, 255, 255, 255, true)
-		end
-	end
-)
+-- Később ide is kerülnek dolgok --
