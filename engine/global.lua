bfdee6f8ce454961f30d8e7adf065713eb2f6a73
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 16:57:52 +0100

    Update global.lua

--- engine/global.lua
+++ engine/global.lua
@@ -4,6 +4,40 @@ modData = {
 	["city"] = "Los Santos",
 }
 
+jobNames = {
+	[0] = "Munkanélküli",
+}
+
+vehicleNames = {
+	[596] = "Opel Corsa",
+}
+
+-- Járművek nevének átküldése --
+function vehicleNamesService(vehicle)
+	if (vehicle) then
+		if (vehicleNames[getElementModel(vehicle)]) then
+			return vehicleNames[getElementModel(vehicle)]
+		else
+			return "ISMERETLEN"
+		end
+	else
+		return false
+	end
+end
+
+-- Munkák nevének átküldése --
+function jobNamesService(player)
+	if (player) then
+		if (jobNames[getElementData(player, "job >> id")]) then
+			return jobNames[getElementData(player, "job >> id")]
+		else
+			return false
+		end
+	else
+		return false
+	end
+end
+
 -- Mod Adatok elküldése --
 function modDatas(data)
 	if (data) then
