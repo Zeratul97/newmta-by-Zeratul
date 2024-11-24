33e87273adf87ce0a5f3a8f76abcacc14b8e59b7
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 17:18:59 +0100

    Update client.lua

--- devsystem/client.lua
+++ devsystem/client.lua
@@ -1,6 +1,6 @@
 -- Fejlesztői mód ki/be kaplcsolása --
 function devMode(cmd)
-	local devSerial = exports["nwmta_dev"]:getDevSerials(localPlayer)
+	local devSerial = exports["devsystem"]:getDevSerials(localPlayer)
 	if (devSerial) or (getElementData(player, "admin >> level") >= 10 ) then
 		if (getDevelopmentMode() == true) then
 			setDevelopmentMode(false)
@@ -13,7 +13,7 @@ addCommandHandler("enableddevmode", devMode)
 
 -- Collision-ök megjelenítése/eltűntetése --
 function showCollisions(cmd)
-	local devSerial = exports["nwmta_dev"]:getDevSerials(localPlayer)
+	local devSerial = exports["devsystem"]:getDevSerials(localPlayer)
 	if (devSerial) or (getElementData(player, "admin >> level") >= 10 ) then
 		showCol (not isShowCollisionsEnabled())
 	end
