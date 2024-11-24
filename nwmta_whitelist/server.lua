85105390faf263d1cf0d517f98a5eb3e5655de6c
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 18:24:19 +0100

    Update server.lua

--- nwmta_whitelist/server.lua
+++ nwmta_whitelist/server.lua
@@ -1,13 +1,13 @@
 -- Játékos felcsatlakozik a szerverre akkor fut le --
 addEventHandler("onPlayerJoin", getRootElement(),
-	function()
-		local banSerial = exports["nwmta_banlist"]:banSerialList(source)
-		local whiteListSerial = exports["nwmta_whitelist"]:getWhiteListSerial(source)
+    function()
+	local banSerial = exports["nwmta_banlist"]:banSerialList(source)
+	local whiteListSerial = exports["nwmta_whitelist"]:getWhiteListSerial(source)
 
-		if not (banSerial) then -- Ha a játékos serial-ja nincs titlva akkor
-			if not (whiteListSerial) then -- Ha a játékos serialja nincs a whitelist tömbjében
-				kickPlayer(source, "nwmta_whitelist", "Szerveren fejlesztés/karbantartás folyik!")
-			end
-		end
+	if not (banSerial) then -- Ha a játékos serial-ja nincs titlva akkor
+	   if not (whiteListSerial) then -- Ha a játékos serialja nincs a whitelist tömbjében
+	        kickPlayer(source, "nwmta_whitelist", "Szerveren fejlesztés/karbantartás folyik!")
+	   end
 	end
+    end
 )
