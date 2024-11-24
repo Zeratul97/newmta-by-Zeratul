56ef9ee7d652d93107ddc1c632f94f10911d8861
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 17:01:28 +0100

    Update global.lua

--- devsystem/global.lua
+++ devsystem/global.lua
@@ -1,29 +1,62 @@
--- Fejlesztői serial-ok --
-devSerials = {
-	["serial"] = true,
-}
-
--- Fejlesztői parancsok --
-devCommands = {
-	["start"] = true,
-	["stop"] = true,
-	["restart"] = true,
-	["refresh"] = true,
-	["refreshall"] = true,
-	["debugscript"] = true,
-	["reloadacl"] = true,
-}
-
 -- Tiltott parancsok --
-disabledCommands = {
+disableCMD = {
 	["register"] = true,
 	["login"] = true,
+	["refreshall"] = true,
+	["stopall"] = true,
 }
 
--- Játékos serial-ja benne van-e a fejlesztői serial-ban --
-function isPlayerSerial(player)
+-- Fejlesztők serial-ja 
+devSerail = {
+	--["serial"] = true, -- Ide a "serial" részre adjátok meg a fejlesztő serial-ját
+}
+
+-- Fejlesztői parancsok listálya --
+devCMD = {
+	-- Alap fejlesztői parancsok --
+	["start"] = true,
+	["stop"] = true,
+	["refresh"] = true,
+	["restart"] = true,
+	["debugscript"] = true,
+	["reloadacl"] = true,
+
+	-- Új fejlesztői parancsok --
+	["getpos"] = true,
+	["enableddevmode"] = true,
+	["enabledcollisions"] = true,
+}
+
+-- Fejlesztői serial ellenőrző funkció --
+function getDevSerials(player)
 	if (player) then
-		if (devSerials[getPlayerSerial(player)]) then
+		if (devSerail[getPlayerSerial(player)]) then
+			return true
+		else
+			return false
+		end
+	else
+		return false 
+	end
+end
+
+-- Tiltott parancsokat ellenőrző funkció --
+function getCMDDisabled(cmd)
+	if (cmd) then
+		if (disableCMD[cmd]) then
+			return true
+		else
+			return false
+		end
+	else
+		return false
+	end
+end
+
+-- Fejlesztői parancsokat ellenőrző funkció --
+function getDeveloperCMD(cmd)
+	if (cmd) then
+		if (devCMD[cmd]) then
 			return true
 		else
 			return false
