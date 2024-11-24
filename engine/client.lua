be81d98501d92eff6f45ff35a8a8d8a68dab36f3
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-11-24 16:56:41 +0100

    Update client.lua

--- engine/client.lua
+++ engine/client.lua
@@ -3,3 +3,39 @@ function setPlayerCursor()
 	showCursor(not isCursorShowing())
 end
 bindKey("m", "up", setPlayerCursor)
+
+addEventHandler("onClientPlayerDamage", root,
+    function(attacker, weapon, bodypart, loss)
+       if (getElementData(source, "account >> login") == true) then
+            if (getElementData(source, "admin >> duty") == true) then
+                cancelEvent()
+            else
+                local text = ""
+                if (getElementType(attacker) == "vehicle") then
+                    local plateText = getVehiclePlateText(attacker)
+                    text = plateText
+
+                elseif (getElementType(attacker) == "player") then
+                    local attackerName = getPlayerName(attacker)
+                    local name = getPlayerName(source)
+                    local weaponName = getWeaponNameFromID(weapon)
+                    local weaponNewName = ""
+                    
+                    if (getWeaponNameFromID(weapon) == "Fist") then
+                        weaponNewName = "Kéz"
+                    elseif (getWeaponNameFromID(weapon) == "Golfclub") then
+                        weaponNewName = "Golfütő"
+                    end
+
+
+
+                    text = "Elkövető: "..attackerName..", Sértett: "..name..", Fegyver: "..weaponNewName
+                end
+                exports["nwmta_admins"]:logMessageClient(text)
+            end
+        else
+            cancelEvent()
+        end
+        
+    end
+)
