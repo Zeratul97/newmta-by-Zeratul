2885f0f6b1ddd6edc4c2e3aebf4c1f5d2462f134
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-09 18:39:38 +0100

    Update global.lua

--- resources/[Protection]/nwmta_banlist/global.lua
+++ resources/[Protection]/nwmta_banlist/global.lua
@@ -1,29 +1,29 @@
 banSerials = {
-	--["serial"] = "Indok",
+    --["serial"] = "Indok",
 }
 
 -- Ellenőrzi hogy a játékos serial-ja banolva van-e, ha igen akkor true(igaz) értéket küld vissza --
 function isSerialBan(player)
-	if (player) then
-		if (banSerials[getPlayerSerial(player)]) then
-			return true
-		else
-			return false
-		end	
+    if (player) then
+	if (banSerials[getPlayerSerial(player)]) then
+	     return true
 	else
-		return false
-	end
+	     return false
+	end	
+    else
+	return false
+    end
 end
 
 -- Vissza küldi a banolás okát --
 function isBanReason(player)
-	if (player) then
-		if (banSerials[getPlayerSerial(player)]) then
-			return banSerials[getPlayerSerial(player)]
-		else
-			return false
-		end
+    if (player) then
+	if (banSerials[getPlayerSerial(player)]) then
+	     return banSerials[getPlayerSerial(player)]
 	else
-		return false
+	     return false
 	end
+    else
+	return false
+    end
 end
