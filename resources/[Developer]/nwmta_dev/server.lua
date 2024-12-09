944dc27d66b38b7cbb764ba354e8f66d728d3bb7
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-09 18:46:56 +0100

    Create server.lua

---
+++ resources/[Developer]/nwmta_dev/server.lua
@@ -0,0 +1,20 @@
+-- Parancsok korlátozása --
+addEventHandler("onPlayerCommand", getRootElement(),
+	function(command)
+
+		local disabledCMD = exports["nwmta_dev"]:isDisabledCMD(command)
+
+		if (disabledCMD) then
+			cancelEvent()
+		else
+			local devCMD = exports["nwmta_dev"]:isDevCMD(command)
+			if (devCMD) then
+				local devSerial = exports["nwmta_dev"]:isPlayerDev(source)
+				if not (devSerial) then
+					cancelEvent()
+				end
+
+			end
+		end
+	end
+)
