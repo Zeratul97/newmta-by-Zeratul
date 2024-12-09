cbbd373c5bd3037609c9b8be93ae567ce68dccc1
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-09 18:17:10 +0100

    Update global.lua

--- resources/[Engine]/nwmta_core/global.lua
+++ resources/[Engine]/nwmta_core/global.lua
@@ -1,19 +1,19 @@
 serverDatas = {
-	["name"] = "NewMTA",
-	["version"] = "v0.0.1",
-	["city"] = "Los Santos",
+    ["name"] = "NewMTA",
+    ["version"] = "v0.0.1",
+    ["city"] = "Los Santos",
 }
 
 
 -- Szerver adatok átküldése --
 function isServerDatas(data)
-	if (data) then
-		if (serverDatas[data]) then
-			return serverDatas[data]
-		else
-			return false
-		end
+    if (data) then
+	if (serverDatas[data]) then
+	    return serverDatas[data]
 	else
-		return false
+	    return false
 	end
+    else
+	return false
+    end
 end
