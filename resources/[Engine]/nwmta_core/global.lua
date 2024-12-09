0d6d0de5838c9e94a0d1037568f75220da998b46
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-09 18:16:24 +0100

    Create global.lua

---
+++ resources/[Engine]/nwmta_core/global.lua
@@ -0,0 +1,19 @@
+serverDatas = {
+	["name"] = "NewMTA",
+	["version"] = "v0.0.1",
+	["city"] = "Los Santos",
+}
+
+
+-- Szerver adatok átküldése --
+function isServerDatas(data)
+	if (data) then
+		if (serverDatas[data]) then
+			return serverDatas[data]
+		else
+			return false
+		end
+	else
+		return false
+	end
+end
