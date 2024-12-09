a42ff178fd4ab8b6dbe63b7f86d5f19a588aab47
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-09 18:42:08 +0100

    Update global.lua

--- resources/[Protection]/nwmta_whitelist/global.lua
+++ resources/[Protection]/nwmta_whitelist/global.lua
@@ -1,16 +1,16 @@
 whiteSerials = {
-	--["serial"] = true,
+    --["serial"] = true,
 }
 
 -- Ellenőrzi hogy a játékos serial-ja szerepel a listában, ha igen akkor true(igaz) értéket küld vissza --
 function isWhiteSerial(player)
-	if (player) then
-		if (whiteSerials[getPlayerSerial(player)]) then
-			return true
-		else
-			return false
-		end
+    if (player) then
+	if (whiteSerials[getPlayerSerial(player)]) then
+	    return true
 	else
-		return false
+	    return false
 	end
+    else
+	return false
+    end
 end
