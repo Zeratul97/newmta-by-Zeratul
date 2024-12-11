b418eae6014fa47eef60904f4baa1b944b0c36e1
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-11 17:17:20 +0100

    Update global.lua

--- resources/[Other]/nwmta_admins/global.lua
+++ resources/[Other]/nwmta_admins/global.lua
@@ -1,1 +1,15 @@
+-- Admin rangok
+local adminsTitle = {"Admin[1]", "Admin[2]", "Admin[3]", "Admin[4]", "Admin[5]", "FőAdmin", "SzuperAdmin", "Rendszergazda", "Tulajdonos", "Fejlesztő"}
 
+-- Játékos admin-e avagy sem --
+function isPlayerAdmin(player)
+    if (player) then
+        if (getElementData(player, "admin")) then
+            return true
+        else
+            return false
+        end
+    else
+        return false
+    end
+end
