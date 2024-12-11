f1562c857ab63d947580d6afbc871d9854161f0e
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-11 17:04:48 +0100

    Update client.lua

--- resources/[Developer]/nwmta_dev/client.lua
+++ resources/[Developer]/nwmta_dev/client.lua
@@ -1,1 +1,17 @@
+-- koordináta lekérdezése --
+function getPlayerPosition(cmd)
+    local devSerial = exports["nwmta_dev"]:isPlayerDev(localPlayer)
+    if (devSerial) then
+        local px, py, pz = getElementPosition(localPlayer)
+        local dim = getElementDimension(localPlayer)
+        local int = getElementInterior(localPlayer)
 
+
+        outputChatBox("X koordináta: "..px, 255, 255, 255, true)
+        outputChatBox("Y koordináta: "..py, 255, 255, 255, true)
+        outputChatBox("Z koordináta: "..pz, 255, 255, 255, true)
+        outputChatBox("Interior: "..int, 255, 255, 255, true)
+        outputChatBox("Dimenzió: "..dim, 255, 255, 255, true)
+    end
+end
+addCommandHandler("getpos", getPlayerPosition)
