c9996a8f848fdc1addd0bb07c0b90f18bd59fc36
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-11 17:05:48 +0100

    Update client.lua

--- resources/[Developer]/nwmta_dev/client.lua
+++ resources/[Developer]/nwmta_dev/client.lua
@@ -15,3 +15,21 @@ function getPlayerPosition(cmd)
     end
 end
 addCommandHandler("getpos", getPlayerPosition)
+-- Fejelsztői mód bekapcsolása --
+function devMode(cmd)
+    --if (getElementData(localPlayer, "login")) then
+        local devSerial = exports["nwmta_dev"]:isPlayerDev(localPlayer)
+        if (devSerial) then
+            if (getDevelopmentMode() == true) then
+                setDevelopmentMode(false)
+                outputChatBox("Fejlesztői mód kikapcsolva!", 255, 255, 255, true)
+            else	
+                setDevelopmentMode(true)
+                outputChatBox("Fejlesztői mód bekapcsolva!", 255, 255, 255, true)
+            end
+        end
+    --else	
+	--	cancelEvent()
+    --end
+end
+addCommandHandler("devmode", devMode)
