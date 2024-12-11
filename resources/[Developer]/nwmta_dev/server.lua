29e01b23a06b7dcf9948e7844ca2ba0fbe4022b0
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-11 17:09:21 +0100

    Update server.lua

--- resources/[Developer]/nwmta_dev/server.lua
+++ resources/[Developer]/nwmta_dev/server.lua
@@ -16,3 +16,23 @@ addEventHandler("onPlayerCommand", getRootElement(),
 	end
     end
 )
+-- Lekérdezi a futó scripteket --
+function getRunningResources(player, cmd)
+    --if (getElementData(localPlayer, "login")) then
+	local devSerial = exports["nwmta_dev"]:isPlayerDev(player)
+	if (devSerial) then
+     		local resourceTable = getResources() -- Lekérdezi az összes szerveren található resource-t
+     		for resourceKey, resourceValue in ipairs(resourceTable) do
+		    local name = getResourceName(resourceValue)
+
+		    if (getResourceState(resourceValue) == "running") then
+                        outputChatBox("Resource név: "..name.." Státusz: Fut", player, 255, 255, 255, true)
+		    end
+     		end
+	end
+
+    --else
+	--cancelEvent()
+    --end
+end
+addCommandHandler("getrunningresource", getRunningResources)
