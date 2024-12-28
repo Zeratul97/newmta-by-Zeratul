0e3703e7731707c863dbe4163f7daf6c92754f2c
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-28 16:21:12 +0100

    Update global.lua

--- resources/[Engine]/nwmta_core/global.lua
+++ resources/[Engine]/nwmta_core/global.lua
@@ -8,6 +8,12 @@ jobList = {
     [0] = "Munkanélküli",
 }
 
+serverColorHEX = {
+    ["log"] = {
+	["blue"] = "#6bbfff",
+	["red"] = "#f94d40",
+    },
+}
 -- Szerver adatok átküldése --
 function isServerDatas(data)
     if (data) then
@@ -28,6 +34,47 @@ function isPlayerJobName(player)
 	    return jobList[getElementData(player, "player >>job")]
 	else
 	    return "Munkanélküli"
+	end
+    else
+	return false
+    end
+end
+
+-- Játékos nevét küldi vissza --
+function isPlayerName(player)
+    if (player) then
+	return getPlayerName(player):gsub("_", " ")
+    else
+	return false
+    end
+end
+
+-- Chat szín vissza küldése(normál chat)--
+function getChatColor(distance)
+    if (distance) then
+	if (distance < 4) then
+            return "#EEEEEE"
+	elseif (distance < 8) then
+            return "#DDDDDD"
+	elseif (distance < 12) then
+            return "#CCCCCC"
+	elseif (distance < 16) then
+            return "#BBBBBB"
+	else
+            return "#AAAAAA"
+	end
+    else
+	return false
+    end
+end
+
+-- Szerver színek átküldése(HEXADECIMÁLIS érték) --
+function isServerColorHEX(type, colorName)
+    if type and colorName then
+	if (serverColorHEX[type][colorName]) then
+            return serverColorHEX[type][colorName]
+	else
+            return "#ffffff"
 	end
     else
 	return false
