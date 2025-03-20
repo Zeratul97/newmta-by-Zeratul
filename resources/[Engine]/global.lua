997989e2610351da9605dbb00b0b95266cb97b09
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-20 16:58:47 +0100

    Update global.lua

--- resources/[Engine]/global.lua
+++ resources/[Engine]/global.lua
@@ -1,19 +1,19 @@
 serverDatas = {
-	["name"] = "NewMTA",
-	["version"] = "v0.0.1",
-	["devVersion"] = "v0.0.0",
-	["city"] = "Los Santos",
-	["developer"] = "Zeratul & Fejlesztő neve",
+    ["name"] = "NewMTA",
+    ["version"] = "v0.0.1",
+    ["devVersion"] = "v0.0.0",
+    ["city"] = "Los Santos",
+    ["developer"] = "Zeratul & Fejlesztő neve",
 }
 
 function isServerData(data)
-	if (data) then
-		if (serverDatas[data]) then
-			return serverDatas[data]
-		else
-			return false
-		end
+    if (data) then
+	if (serverDatas[data]) then
+	     return serverDatas[data]
 	else
-		return false
+	     return false
 	end
+    else
+	return false
+    end
 end
