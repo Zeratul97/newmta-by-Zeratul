73b3e51a7d382e9626e2844dc717f9fa3771690b
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-20 16:56:44 +0100

    Update client.lua

--- resources/[Engine]/client.lua
+++ resources/[Engine]/client.lua
@@ -1,1 +1,9 @@
-
+-- Kurzor megjelenítése --
+function cursorShowing()
+    if (isCursorShowing()) then
+		    showCursor(false)
+    else
+		    showCursor(true)
+    end
+end
+bindKey("m", "up", cursorShowing)
