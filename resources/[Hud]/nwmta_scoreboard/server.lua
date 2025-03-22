cbff41378f153f868b36b6adc8c91d1171c08a72
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 16:00:36 +0100

    Update server.lua

--- resources/[Hud]/nwmta_scoreboard/server.lua
+++ resources/[Hud]/nwmta_scoreboard/server.lua
@@ -4,28 +4,28 @@ function playerConnection()
     local slot = nil
 
     for i=1, 400 do
-		    if (players[i] == nil) then
-			      slot = i
-			      break
-		    end
+	if (players[i] == nil) then
+	    slot = i
+	    break
+	end
     end
-	players[slot] = source
-	setElementData(source, "player >> id", slot)
+    players[slot] = source
+    setElementData(source, "player >> id", slot)
 end
 addEventHandler("onPlayerJoin", getRootElement(), playerConnection)
 
 function resourceStart()
-	for key, value in ipairs(getElementsByType("player")) do
-		players[key] = value
-		setElementData(value, "player >> id", key)
-	end
+    for key, value in ipairs(getElementsByType("player")) do
+	players[key] = value
+	setElementData(value, "player >> id", key)
+    end
 end
 addEventHandler("onResourceStart", getRootElement(), resourceStart)
 
 function playerQuit()
-	local slot = getElementData(source, "player >> id")
-	if (slot) then
-		players[slot] = nil
-	end
+    local slot = getElementData(source, "player >> id")
+    if (slot) then
+	players[slot] = nil
+    end
 end
 addEventHandler("onPlayerQuit", getRootElement(), playerQuit)
