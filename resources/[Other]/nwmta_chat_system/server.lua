b47c28df5cf95fd3c6174ac941bd839241802841
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 14:00:31 +0100

    Update server.lua

--- resources/[Other]/nwmta_chat_system/server.lua
+++ resources/[Other]/nwmta_chat_system/server.lua
@@ -1,14 +1,14 @@
 addEventHandler("onPlayerChat", getRootElement(),
     function(message, messageType)
-		    cancelEvent()
-		    if (messageType == 0) then
-			      --if (getElementData(source, "login")) then
-				          if not (isPedDead(source)) then
-					           local x, y, z = getElementPosition(source)
-					           local int = getElementInterior(source)
-					           local dim = getElementDimension(source)
-					           for i, players in ipairs(getElementsByType("player")) do
-						             --if (getElementData(players, "login")) then
+        cancelEvent()
+        if (messageType == 0) then
+	    --if (getElementData(source, "login")) then
+                if not (isPedDead(source)) then
+                   local x, y, z = getElementPosition(source)
+                   local int = getElementInterior(source)
+                   local dim = getElementDimension(source)
+                   for i, players in ipairs(getElementsByType("player")) do
+			--if (getElementData(players, "login")) then
 							               local pX, pY, pZ = getElementPosition(players)
 							               local pInt = getElementInterior(players)
 							               local pDim = getElementDimension(players)
@@ -24,10 +24,10 @@ addEventHandler("onPlayerChat", getRootElement(),
 									                   end
 								                 end
 							               end
-						             --end
-					           end
-				          end
-			      --end
-		    end
+			--end
+                   end
+                end
+	    --end
+        end
     end
 )
