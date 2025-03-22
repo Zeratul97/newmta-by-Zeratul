c89e72a43e6b0b3d50d96dc46060b51802295530
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 14:01:28 +0100

    Update server.lua

--- resources/[Other]/nwmta_chat_system/server.lua
+++ resources/[Other]/nwmta_chat_system/server.lua
@@ -9,21 +9,21 @@ addEventHandler("onPlayerChat", getRootElement(),
                    local dim = getElementDimension(source)
                    for i, players in ipairs(getElementsByType("player")) do
 			--if (getElementData(players, "login")) then
-							               local pX, pY, pZ = getElementPosition(players)
-							               local pInt = getElementInterior(players)
-							               local pDim = getElementDimension(players)
+			    local pX, pY, pZ = getElementPosition(players)
+			    local pInt = getElementInterior(players)
+			    local pDim = getElementDimension(players)
 
-							               if (int == pInt) then
-								                 if (dim == pDim) then
-									                   local distance = getDistanceBetweenPoints3D(x, y, z, pX, pY, pZ)
-									                   if (distance < 30) then
-										                     local chatColor = exports["nwmta_core"]:getChatColor(distance)
-										                     local playerName = exports["nwmta_core"]:isPlayerName(source)
+			    if (int == pInt) then
+				if (dim == pDim) then
+				    local distance = getDistanceBetweenPoints3D(x, y, z, pX, pY, pZ)
+				    if (distance < 30) then
+					local chatColor = exports["nwmta_core"]:getChatColor(distance)
+					local playerName = exports["nwmta_core"]:isPlayerName(source)
 
-										                     outputChatBox(chatColor..""..playerName.." mondja: "..message, players, 255, 255, 255, true)
-									                   end
-								                 end
-							               end
+					outputChatBox(chatColor..""..playerName.." mondja: "..message, players, 255, 255, 255, true)
+				    end
+				end
+			    end
 			--end
                    end
                 end
