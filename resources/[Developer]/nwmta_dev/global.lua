70e005b90412d96a9ce3c7dae8282df47743ab6c
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 14:18:56 +0100

    Update global.lua

--- resources/[Developer]/nwmta_dev/global.lua
+++ resources/[Developer]/nwmta_dev/global.lua
@@ -1,1 +1,64 @@
+-- Titlott parancsok --
+disabledCMD = {
+	   ["login"] = true,
+	   ["register"] = true,
+	   ["refreshall"] = true,
+}
 
+-- Fejlesztői parancsok --
+devCMD = {
+	["start"] = true,
+	["stop"] = true,
+	["restart"] = true,
+	["refresh"] = true,
+	["debugscript"] = true,
+	["reloadacl"] = true,
+
+	-- Új parancsok --
+	["getpos"] = true,
+	["devmode"] = true,
+	["collisionshowing"] = true,
+}
+
+devSerials = {
+	["3EEE639776EC134366D2D005508912F3"] = true,
+}
+
+-- Ellenőrzi hogy a parancs tiltva van-e --
+function isDisabledCMD(cmd)
+	if (cmd) then
+		if (disabledCMD[cmd]) then
+			return true
+		else
+			return false
+		end
+	else
+		return false
+	end
+end
+
+-- Ellenőrzi hogy a parancs fejlesztői parancs-e --
+function isDevCMD(cmd)
+	if (cmd) then
+		if (devCMD[cmd]) then
+			return true
+		else
+			return false
+		end
+	else
+		return false
+	end
+end
+
+-- Ellenőrzi hogy a játékos fejlesztő-e vagy sem --
+function isPlayerDev(player)
+	if (player) then
+		if (devSerials[getPlayerSerial(player)]) then
+			return true
+		else
+			return false
+		end
+	else
+		return false
+	end
+end
