2d15de339ea1ec832efa36158592391d92445c38
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 16:56:14 +0100

    Update client.lua

--- resources/[Hud]/nwmta_infobox/client.lua
+++ resources/[Hud]/nwmta_infobox/client.lua
@@ -1,1 +1,24 @@
+local screen = {}
+screen.x, screen.y = guiGetScreenSize();
 
+local anims, builtins = {}, {"Linear", "InQuad", "OutQuad", "InOutQuad", "OutInQuad", "InElastic", "OutElastic", "InOutElastic", "OutInElastic", "InBack", "OutBack", "InOutBack", "OutInBack", "InBounce", "OutBounce", "InOutBounce", "OutInBounce", "SineCurve", "CosineCurve"}
+
+--------------------- // Hiba iconok és a meghívási név // ---------------------
+local itypes = {
+    error = "files/images/error.png",
+    info = "files/images/info.png",
+    success = "files/images/success.png",
+    warning = "files/images/warning.png",
+}
+
+local font = dxCreateFont("files/fonts/roboto.ttf", 9)
+
+local messages = {};
+addEvent("infobox", true)
+addEventHandler("infobox", localPlayer,
+    function(message, itype) 
+		    --createInfoBox(message, itype, playSound("infobox/files/sounds/"..itype..".mp3"))
+		    createInfoBox(message, itype)
+		    return
+    end
+)
