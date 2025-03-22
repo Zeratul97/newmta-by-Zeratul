b665dc0a5b18e3ff0fd72b47f7bdcfda5729c2c0
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 15:50:50 +0100

    Update global.lua

--- resources/[Engine]/global.lua
+++ resources/[Engine]/global.lua
@@ -19,3 +19,45 @@ function isServerData(data)
 	return false
     end
 end
+
+function findPlayer(sourcePlayer, id)
+
+	if id == "*" or utfSub(id, 1, 1) == "*" then
+		return sourcePlayer
+	end
+
+	local table = {}
+
+	for key, value in pairs(getElementsByType("player")) do
+
+		local name = getPlayerName(value)
+		logged = false
+
+		if (utfSub(name, 1, #id) == string.lower(id)) and not logged then
+			table[#table+1] = {value, name}
+			logged = true
+		end
+
+		local adminName = getElementData(value, "admin >> nick") or "ISMERETLEN"
+
+		if (utfSub(adminName, 1, #id) == string.lower(id)) and not logged then
+			table[#table+1] = {value, adminName}
+			logged = true
+		end
+
+		local number = tonumber(getElementData(value, "player >> id"))
+		local number2 = tonumber(id)
+
+		if (number == number2) and not logged then
+			table[#table+1] = {value, adminName}
+			logged = true
+		end
+
+		if #table == 0 then
+
+		elseif #table == 1 then
+			return table[1][1]
+		end
+
+	end
+end
