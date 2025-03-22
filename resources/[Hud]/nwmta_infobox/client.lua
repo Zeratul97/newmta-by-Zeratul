7bedd12e32d24108c740dc9fd867610c2bf2f02a
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 16:58:37 +0100

    Update client.lua

--- resources/[Hud]/nwmta_infobox/client.lua
+++ resources/[Hud]/nwmta_infobox/client.lua
@@ -22,3 +22,41 @@ addEventHandler("infobox", localPlayer,
 	return
     end
 )
+
+--------------------------------------------------------------------------------
+------------------------------ Koordináták és szélesség és magasság ---------------
+local infobox = {}
+infobox.w = 200	--Mező szélesség
+infobox.h = 33	--Mező magassága
+infobox.y = 50 -- Magasság
+infobox.x = screen.x/2 - infobox.w/2 --Oldalra pozícionálás
+-----------------------------------------------------------------------------------
+
+function createInfoBox(message, itype)
+    local id = #messages+1
+    table.insert(messages,{
+        id = id,
+        message = message, 
+        itype = itype,
+        x = infobox.x,
+        y = infobox.y,
+        w = infobox.h,
+        h = infobox.h,
+        dWidth = infobox.h + dxGetTextWidth(message, 1, font) + 10,
+        opacity = 0, --0-1 közötti érték
+        animState = false,
+        }) -- Message táblázatba visz egy üzenetet
+
+    playSound("files/sounds/"..itype..".mp3", false)
+    createAnimation(0, 1, 4, 300, function(animation) messages[id].opacity = animation end, function()
+	createAnimation(infobox.h, messages[id].dWidth, 4, 300, function(animation) messages[id].w = animation end, function()
+	    setTimer(function()
+                createAnimation(messages[id].dWidth, infobox.h, 4, 300, function(animation) messages[id].w = animation end, function()
+		     createAnimation(1, 0, 4, 300, function(animation) messages[id].opacity = animation end, function()
+			  messages[id] = nil
+		     end)
+                end)
+	    end, 2000, 1)
+	end)
+    end)
+end
