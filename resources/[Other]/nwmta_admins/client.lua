13aaf2ff6ec304be66e3c3dcafb42d79e7546615
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 06:49:55 +0100

    Update client.lua

--- resources/[Other]/nwmta_admins/client.lua
+++ resources/[Other]/nwmta_admins/client.lua
@@ -1,1 +1,15 @@
+local modName = exports["nwmta_engine"]:isServerData("name") or "NewMTA"
 
+function logMessage(msg)
+	local admin = exports["nwmta_admins"]:isPlayerAdmin(localPlayer)
+	if (admin) then
+		outputChatBox("["..modName.." - Log]:"..msg, 255, 255, 255, true)
+	end
+end
+
+addEvent("logMessageServer", true)
+addEventHandler("logMessageServer", getRootElement(),
+	function(msg)
+		logMessage(msg)
+	end
+)
