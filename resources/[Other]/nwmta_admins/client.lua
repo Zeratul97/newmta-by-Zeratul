585336587fc60a130eae7dadc91bf9050be0eb74
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 13:50:54 +0100

    Update client.lua

--- resources/[Other]/nwmta_admins/client.lua
+++ resources/[Other]/nwmta_admins/client.lua
@@ -1,15 +1,32 @@
 local modName = exports["nwmta_engine"]:isServerData("name") or "NewMTA"
 
-function logMessage(msg)
-    local admin = exports["nwmta_admins"]:isPlayerAdmin(localPlayer)
-    if (admin) then
-	outputChatBox("["..modName.." - Log]:"..msg, 255, 255, 255, true)
-    end
+-- Log üzenet --
+function logMessage(message)
+	local devSerial = exports["nwmta_dev"]:isPlayerDev(localPlayer)
+	if (getElementData(localPlayer, "admin")) or (devSerial) then
+		outputChatBox("["..modName.." - LOG]: "..message, 255, 255, 255, true)
+	end
 end
 
-addEvent("logMessageServer", true)
-addEventHandler("logMessageServer", getRootElement(),
-    function(msg)
-	logMessage(msg)
-    end
+-- Szerver oldali Log --
+addEvent("serverLogMessage", true)
+addEventHandler("serverLogMessage", root,
+	function(message)
+		logMessage(message)
+	end
+)
+
+-- Admin napló üzenet --
+function adminDiaryMessage(message)
+	local devSerial = exports["nwmta_dev"]:isPlayerDev(localPlayer)
+	if (getElementData(localPlayer, "admin")) or (devSerial) then
+		outputChatBox("["..modName.." - Admin napló]: "..message, 255, 255, 255, true)
+	end
+end
+
+addEvent("serverAdminDiaryMessage", true)
+addEventHandler("serverAdminDiaryMessage", root,
+	function(message)
+		adminDiaryMessage(message)
+	end
 )
