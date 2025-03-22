b0fd1fe17515c7f6a5170d72138a1a6782625601
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 13:54:35 +0100

    Update client.lua

--- resources/[Other]/nwmta_chat_system/client.lua
+++ resources/[Other]/nwmta_chat_system/client.lua
@@ -1,15 +1,15 @@
 -- /me parancs --
 function sendMeMessage(cmd, ...)
-	--if (getElementData(localPlayer, "login")) then
-		if not (...) then
-			outputChatBox("Szintaxis: /"..cmd.." [Cselekvés]", 255, 194, 14)
-		else
-			if not (isPedDead(localPlayer)) then
-				local message = table.concat({...}, " ")
-				triggerServerEvent("sendMeMessageServer", resourceRoot, localPlayer, message)
-			end
+    --if (getElementData(localPlayer, "login")) then
+	  if not (...) then
+		outputChatBox("Szintaxis: /"..cmd.." [Cselekvés]", 255, 194, 14)
+	  else
+		if not (isPedDead(localPlayer)) then
+		    local message = table.concat({...}, " ")
+		    triggerServerEvent("sendMeMessageServer", resourceRoot, localPlayer, message)
 		end
-	--end
+	  end
+    --end
 end
 addCommandHandler("me", sendMeMessage)
 
