2bf9e285fa217b618c27e278eaa5ca64a33860ad
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 14:16:19 +0100

    Update client.lua

--- resources/[Developer]/nwmta_dev/client.lua
+++ resources/[Developer]/nwmta_dev/client.lua
@@ -1,18 +1,18 @@
 -- koordináta lekérdezése --
 function getPlayerPosition(cmd)
-     local devSerial = exports["nwmta_dev"]:isPlayerDev(localPlayer)
-     if (devSerial) then
-		     local px, py, pz = getElementPosition(localPlayer)
-		     local dim = getElementDimension(localPlayer)
-		     local int = getElementInterior(localPlayer)
+    local devSerial = exports["nwmta_dev"]:isPlayerDev(localPlayer)
+    if (devSerial) then
+	local px, py, pz = getElementPosition(localPlayer)
+	local dim = getElementDimension(localPlayer)
+	local int = getElementInterior(localPlayer)
 
 
-		     outputChatBox("X koordináta: "..px, 255, 255, 255, true)
-		     outputChatBox("Y koordináta: "..py, 255, 255, 255, true)
-		     outputChatBox("Z koordináta: "..pz, 255, 255, 255, true)
-		     outputChatBox("Interior: "..int, 255, 255, 255, true)
-		     outputChatBox("Dimenzió: "..dim, 255, 255, 255, true)
-     end
+	outputChatBox("X koordináta: "..px, 255, 255, 255, true)
+	outputChatBox("Y koordináta: "..py, 255, 255, 255, true)
+	outputChatBox("Z koordináta: "..pz, 255, 255, 255, true)
+	outputChatBox("Interior: "..int, 255, 255, 255, true)
+	outputChatBox("Dimenzió: "..dim, 255, 255, 255, true)
+    end
 end
 addCommandHandler("getdevpos", getPlayerPosition)
 
