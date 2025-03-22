4ea720e60bbf4123d060a570f1ec571a34b56ced
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-22 06:06:35 +0100

    Update server.lua

--- resources/[Engine]/server.lua
+++ resources/[Engine]/server.lua
@@ -1,1 +1,16 @@
 
+addEventHandler("onResourceStart", getRootElement(),
+	function(resource)
+		local serverDataName = exports["nwmta_engine"]:isServerData("name") or "NewMTA"
+		local serverDataVersion = exports["nwmta_engine"]:isServerData("version") or "v0.0.1"
+		local serverDataCity = exports["nwmta_engine"]:isServerData("city") or "Los Santos"
+
+		setGameType(serverDataName.." "..serverDataVersion)
+		setMapName(serverDataCity)
+
+		local realtime = getRealTime()
+
+   		setTime(realtime.hour, realtime.minute)
+    	setMinuteDuration(60000)
+	end
+)
