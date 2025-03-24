a264f7a3fc23640754b997ed7cec4743985d52cc
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-24 09:54:24 +0100

    Update nwmta.sql

--- nwmta.sql
+++ nwmta.sql
@@ -1,1 +1,17 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : Házi szerver
+Source Server Version : 50508
+Source Host           : localhost:3306
+Source Database       : newmta
+
+Target Server Type    : MYSQL
+Target Server Version : 50508
+File Encoding         : 65001
+
+Date: 2025-03-24 09:53:52
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
 
