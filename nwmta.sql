80b61d02a11a1dffd3dbe7ca7739b25e808b6ce0
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-03-24 10:49:23 +0100

    Update nwmta.sql

--- nwmta.sql
+++ nwmta.sql
@@ -15,3 +15,21 @@ Date: 2025-03-24 09:53:52
 
 SET FOREIGN_KEY_CHECKS=0;
 
+-- ----------------------------
+-- Table structure for `atms`
+-- ----------------------------
+DROP TABLE IF EXISTS `atms`;
+CREATE TABLE `atms` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `xPos` int(255) DEFAULT NULL,
+  `yPos` int(255) DEFAULT NULL,
+  `zPos` int(255) DEFAULT NULL,
+  `interior` int(255) DEFAULT NULL,
+  `dimension` int(255) DEFAULT NULL,
+  `rotation` int(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- ----------------------------
+-- Records of atms
+-- ----------------------------
