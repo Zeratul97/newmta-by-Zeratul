2c8781b5b6203abe1194b8b4a2eb53ee05bee455
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2025-08-24 17:48:45 +0200

    Delete resources directory

--- resources/[Engine]/client.lua
+++
@@ -1,9 +0,0 @@
--- Kurzor megjelenítése --
-function cursorShowing()
-    if (isCursorShowing()) then
-	showCursor(false)
-    else
-	showCursor(true)
-    end
-end
-bindKey("m", "up", cursorShowing)
