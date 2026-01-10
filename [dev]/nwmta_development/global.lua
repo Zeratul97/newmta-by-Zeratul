f1728419c4ec256aa2ea95b135248041cce9ba00
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2026-01-10 09:52:37 +0100

    Create global.lua

---
+++ [dev]/nwmta_development/global.lua
@@ -0,0 +1,17 @@
+-- Fejlesztői serial-ok listája --
+devSerials = {
+	["fejlesztő serial-ja"] = true, --Lenovo IdeaPad Gaming 3(Zeratul)
+}
+
+-- Fejlesztő-e a játékos avagy sem --
+function getDevSerial(player)
+	  if (player) then
+		    if (devSerials[getPlayerSerial(player)]) then
+			      return true
+		    else
+			      return false
+		    end
+	  else
+		    return false
+	  end
+end
