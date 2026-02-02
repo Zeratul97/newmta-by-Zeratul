42b7187cc87db7e08a64c3927b836b219b25a096
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2026-02-02 19:32:05 +0100

    Update client.lua

--- resources/[NWMTA]/[Engine]/nwmta_engine/client.lua
+++ resources/[NWMTA]/[Engine]/nwmta_engine/client.lua
@@ -1,7 +1,7 @@
 -- Játékos és admin védelem --
 addEventHandler("onClientPlayerDamage", getRootElement(),
 	function(attacker, damage_causing, bodypart, loss)
-		if (getElementData(source, "admin >> duty") == true) or (getElementData(source, "player >> login") == false) then
+		if (getElementData(source, "admin >> duty") == true) or (getElementData(source, "player >> login") == false) then -- Admin szolgálatban nem semsződik/Játékos, ha nincs bejelentkezve nem sebződik --
 			cancelEvent()
 		end
 	end
